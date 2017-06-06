117146ef19d6e4bec23639999aba4eb1b7c34da8
Sabella Rose Jaroszewski <blueoceanz1120@yahoo.com>
2017-06-06 21:05:47 +0000

    base and first bonus finished

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,11 @@
-# Write your code here!
+def speak_to_grandma(string)
+    if string == string.upcase
+        year = rand(1930..1950)
+        "NO, NOT SINCE #{year}!"
+    else
+        "HUH?! SPEAK UP, SONNY!"
+    end
+end
 
+
+puts speak_to_grandma("HELLO")
