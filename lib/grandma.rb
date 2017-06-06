5bbd432b85d3042d935de02f35b61d0df2eb6c06
Sabella Rose Jaroszewski <blueoceanz1120@yahoo.com>
2017-06-06 21:15:15 +0000

    base, bonus, and double bonus complete

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,11 +1,18 @@
 def speak_to_grandma(string)
-    if string == string.upcase
+    if string == string.upcase and string != "BYE"
         year = rand(1930..1950)
         "NO, NOT SINCE #{year}!"
+    elsif string == string.upcase and string == "BYE"
+        exit
     else
         "HUH?! SPEAK UP, SONNY!"
     end
 end
 
+reply = "nothing"
 
-puts speak_to_grandma("HELLO")
+while reply != "BYE"
+    puts "What would you like to say to Granny?"
+    reply = gets.chomp
+    puts speak_to_grandma(reply)
+end
